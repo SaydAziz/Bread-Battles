5eaabc24662dcbe6fc87f8d6ea82bf3625a6c110
Saidazizkhon Saydaminov <sayd.aziz02@gmail.com>
2021-06-19 04:38:19 -0400

    Added Photon Lobby connection

---
+++ Assets/Scripts/Launcher.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class Launcher : MonoBehaviourPunCallbacks
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Connecting to Master Server");
+        PhotonNetwork.ConnectUsingSettings(); 
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Connected to Master Server");
+        PhotonNetwork.JoinLobby();
+    }
+
+    public override void OnJoinedLobby()
+    {
+        Debug.Log("Joined Lobby");
+    }
+
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
